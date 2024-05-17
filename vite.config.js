c50bfecfdd57f1d514a9d9e69eeda2081d8b71da
Steven Hiscocks <steven@hiscocks.me.uk>
2024-05-17 17:20:06 +0100

    Add initial changes for PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default {
   base: "/pota-gb-map/",
-  build: {
-    sourcemap: true,
-  }
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      pwaAssets: {},
+      manifest: {
+        name: 'UK Portable Ham Map',
+        short_name: 'UK Ham Map',
+        description: 'A map for UK based portable amateur radio operators, overlaying award programme references and associated land designations',
+        theme_color: '#cbcbcb',
+      }
+    })
+  ]
 }
