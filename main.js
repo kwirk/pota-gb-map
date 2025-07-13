85a3d550e3b761787b81e760ed7a45b935aaac00
Ian Renton <ian@ianrenton.com>
2025-07-13 12:23:31 +0100

    URL fix

--- main.js
+++ main.js
@@ -838,7 +838,7 @@ esriWorldImageryGroup.once('change:visible', () => {
       attributions: ['Powered by Esri',
         'Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community'],
       attributionsCollapsible: false,
-      url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
       maxZoom: 23
     })
   }));
